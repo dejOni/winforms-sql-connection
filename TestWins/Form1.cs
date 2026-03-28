b46024d6c56cd5d60417043e08147fc50d9eecd2
dejOni <jamesandreifalar@gmail.com>
2026-03-28 23:38:50 +0800

    Refactor Form1 by removing student CRUD logic
    Removed student creation, update, and deletion logic from button click events.

--- TestWins/Form1.cs
+++ TestWins/Form1.cs
@@ -1,12 +1,11 @@
 using System.Drawing.Text;
 using TestWins.Controller;
-using TestWins.Model;
 
 namespace TestWins;
 
 public partial class Form1 : Form
 {
-    // business
+    //business
     private readonly StudentController controller = new StudentController();
 
     public Form1()
@@ -22,50 +21,21 @@ public partial class Form1 : Form
 
     private void btnAdd_Click(object sender, EventArgs e)
     {
-        var student = new Student
-        {
-            Id = int.Parse(txtId.Text),
-            Name = txtName.Text,
-            Course = txtCourse.Text,
-            Year = int.Parse(txtYear.Text)
-        };
 
-        controller.create(student);
-        loadData();
     }
 
     private void btnUpdate_Click(object sender, EventArgs e)
     {
-        var student = new Student
-        {
-            Id = int.Parse(txtId.Text),
-            Name = txtName.Text,
-            Course = txtCourse.Text,
-            Year = int.Parse(txtYear.Text)
-        };
 
-        controller.update(student);
-        loadData();
     }
 
     private void btnDelete_Click(object sender, EventArgs e)
     {
-        int id = int.Parse(txtId.Text);
 
-        controller.delete(id);
-        loadData();
     }
 
-    private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+    private void dataGridView1_CellClick(object sender, EventArgs e)
     {
-        if (e.RowIndex >= 0)
-        {
-            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
 
-            txtId.Text = row.Cells["Id"].Value.ToString();
-            txtName.Text = row.Cells["Name"].Value.ToString();
-            txtCourse.Text = row.Cells["Course"].Value.ToString();
-            txtYear.Text = row.Cells["Year"].Value.ToString();
-        }
     }
 }
